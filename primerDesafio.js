cc6b877da986fa719b80dedbfa9b553099efc582
Michelle Diaz Copello <m.diazcopello@gmail.com>
2023-11-22 00:06:00 -0300

    Consignas

--- primerDesafio.js
+++ primerDesafio.js
@@ -1,68 +1,29 @@
-// Definir clase TicketManager, el cual tendrá un arreglo de eventos que iniciará vacío
-// La clase debe contar con una variable privada “precioBaseDeGanancia”, la cual añadirá un costo adicional al precio de cada evento.
-// Debe contar con el método “getEventos” El cual mostrará los eventos guardados.
-// Debe contar con el método “agregarEvento” El cual recibirá los siguientes parámetros: nombre, lugar, precio (deberá agregarse un 0.15 del valor original), capacidad (50 por defecto), fecha (hoy por defecto)
-// El método deberá crear además el campo id autoincrementable y el campo “participantes” que siempre iniciará con un arreglo vacío.
-// Debe contar con un método “agregarUsuario” El cual recibirá: id del evento (debe existir, agregar validaciones), id del usuario
-// El método debe evaluar que el evento exista y que el usuario no haya estado registrado previamente (validación de fecha y capacidad se evitará para no alargar el reto)
-// Si todo está en orden, debe agregar el id del usuario en el arreglo “participantes” de ese evento.
-// Debe contar con un método “ponerEventoEnGira” El cual recibirá: id del evento, nueva localidad, nueva fecha.
-// El método debe copiar el evento existente, con una nueva localidad, nueva fecha, nuevo id y sus participantes vacíos (Usar spread operator para el resto de las propiedades)
-
-class TicketManager {
-    #precioBaseDeGanancia = 0.15;
+class ProductManager {
     constructor () {
-        this.eventos = [];
+        this.products = [];
     }
 
-    static id = 0;
-
-    getEventos () {
-        return this.eventos;
-    }
-
-    agregarEventos ( nombre, lugar, precio, capacidad = 50, fecha = new Date () ) {
-        TicketManager.id++;
-        const evento = {
-            id: TicketManager.id,
-            nombre,
-            lugar,
-            precio,
-            capacidad,
-            fecha,
-            participantes: [],
+    addProduct ( title, description, price, thumbnail, code, stock) {
+        const product = {
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
+            stock,
         }
-        this.eventos.push ( evento );
-    }
-
-    getEvento ( eventoId ) {
-        return this.eventos.find ( evento => evento.id === eventoId );
-    }
-    agregarUsuario ( eventoId, usuarioId ) {
-        const evento = this.getEvento ( eventoId );
-        if ( evento ) {
-            if ( !evento.participantes.includes ( usuarioId ) ) {
-                evento.participantes.push ( usuarioId );
-            }
-        }
-    }
-
-    ponerEventoEnGira ( eventoId, nuevaLocalidad, nuevaFecha ) {
-        TicketManager.id++;
-        const evento = this.getEvento ( eventoId );
-        if ( evento ) {
-            const nuevoEvento = {
-                ...evento,
-                id: TicketManager.id,
-                localidad: nuevaLocalidad,
-                fecha: nuevaFecha,
-            }
-            this.eventos.push ( nuevoEvento );
-        }
-    }
+        this.products.push ( product );
+    }    
 }
 
-const ticketManager = new TicketManager ();
-ticketManager.agregarEventos ( "Recital de Andrea Boccelli", "Luna Park", 500, 10000 );
-const eventos = ticketManager.getEventos ();
-console.log ( eventos );
+// Validar que no se repita el campo “code” y que todos los campos sean obligatorios
+// Al agregarlo, debe crearse con un id autoincrementable
+// Debe contar con un método “getProducts” el cual debe devolver el arreglo con todos los productos creados hasta ese momento
+// Debe contar con un método “getProductById” el cual debe buscar en el arreglo el producto que coincida con el id
+// En caso de no coincidir ningún id, mostrar en consola un error “Not found”
+
+
+// Formato del entregable
+
+// Archivo de Javascript listo para ejecutarse desde node.
+// Proceso de testing de este entregable ✅
